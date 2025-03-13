dffec9ac984a0b58b8dd41b6006d30c1deef5eb1
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-03-13 11:46:57 +0600

    update

---
+++ Auth-System/src/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
+import Login from "./Component/Login.jsx";
+import Registration from "./Component/Registration.jsx";
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/registration" element={<Registration />} />
+    </Routes>
+  </BrowserRouter>
+);
