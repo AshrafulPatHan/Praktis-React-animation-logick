5d71aa5f49e6059bc071535501c2a33b36b1d053
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-04-16 07:44:25 +0600

    image provlem is sovlv

--- Praktis-React-Slider/src/Components/Baner/Baner3.jsx
+++ Praktis-React-Slider/src/Components/Baner/Baner3.jsx
@@ -1,10 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Snowfall from 'react-snowfall'
 import snowflake1 from '../../assets/snowflake.png'
 import snowflake2 from '../../assets/icons8-snowflake-50.png'
 
+
 const Baner3 = () => {
     const [snow,setSnow] = useState(true)
+    const [snowImages, setSnowImages] = useState([])
+
+    useEffect(() => {
+        const img1 = new Image()
+        img1.src = snowflake1
+
+        const img2 = new Image()
+        img2.src = snowflake2
+
+        setSnowImages([img1, img2])
+    }, [])
+
     const handelToggling = ()=>{
         setSnow(!snow)
     }
@@ -13,19 +26,19 @@ const Baner3 = () => {
             <div className='h-screen flex justify-center items-center ' style={{ background: '#282c34', position: 'relative' }}>
                 {snow ? (
                     <Snowfall 
-                    snowflakeCount={100} 
+                    snowflakeCount={100}
                     speed={[1, 3]}
-                    wind={[0, 2]} 
-                    radius={[1, 3]}
-                    images={[snowflake1, snowflake2]}
+                    wind={[0, 2]}
+                    radius={[5, 19]}
+                    images={snowImages}
                     />
                 ):(
                 <div></div>
                 )}
-                
                 <button  onClick={()=> handelToggling()}
                 className='bg-blue-400 p-3 rounded-3xl '>
-                    Stop Snow Falling
+                    {snow ? "Stop Snow Falling" : "Start Snow Falling" }
+                    
                 </button>
             </div>
         </div>
