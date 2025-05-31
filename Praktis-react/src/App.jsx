3cfa16b9511ca8d7572e01fac4a2159be05af778
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-05-31 21:57:31 +0600

    set up

--- Praktis-react/src/App.jsx
+++ Praktis-react/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { Link } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      <h2>Hello World</h2>
-    </>
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <Link to='/bg' className='text-blue-600 underline'>Go to BG/</Link>
+    </div>
   )
 }
 
--- Praktis-react/src/main.jsx
+++ Praktis-react/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
+import Bg from "./page/Bg.jsx";
 
 const root = document.getElementById("root");
 
@@ -9,6 +10,7 @@ ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path="/bg" element={<Bg />} />
     </Routes>
   </BrowserRouter>
 );
