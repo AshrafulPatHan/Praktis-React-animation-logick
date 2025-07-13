dc7a86ca4cc07a120ebece90bd7d2f7ec7168e31
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-07-13 12:28:31 +0600

    add button

--- src/page/css/Css.jsx
+++ src/page/css/Css.jsx
@@ -2,12 +2,15 @@ import './app.css';
 
 const Css = () => {
     return (
-        <div>
+        <div className='flex flex-col items-center '>
             {/* gradiedent */}
             <div>
-                <div>
-                    <button className=''>
+                <div className='flex flex-col gap-5'>
+                    <button className='btn-border-image'>
                         My Button
+                    </button>
+                    <button className='border-image-btn w-[220px] h-[60px] bg-[#131212] text-white '>
+                        Our Button
                     </button>
                 </div>
             </div>
