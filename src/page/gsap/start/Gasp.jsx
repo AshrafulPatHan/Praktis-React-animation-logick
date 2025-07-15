7ff72b5b818dd0b0f59726e724f5c1a7c9db45c5
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-07-15 10:15:01 +0600

    upate

--- src/page/gsap/start/Gasp.jsx
+++ src/page/gsap/start/Gasp.jsx
@@ -13,12 +13,28 @@ const Gasp = () => {
             duration:0.9,
             ease:"back.out",
         })
+    }) 
+    // gsap.FromTo() method
+    useGSAP(()=>{
+        gsap.fromTo('#blue-fromDox-gsap',{
+            x:-250
+        },{
+            x:250,
+            repeat:-1,
+            yoyo:true,
+            rotate:360,
+            borderRadius:100,
+            duration:1.5,
+            ease:"bounce",
+        })
     })
 
     return (
         <main className="flex flex-col items-center">
             <h1>Gsap</h1>
             <div className="bg-blue-500 w-48 h-48 rounded-2xl mt-44 " id="blue-box-gsap"></div>
+            <div className="bg-[#fff0] w-48 h-48 rounded-2xl mt-44 " ></div>
+            <div className="bg-blue-500 w-48 h-48 rounded-2xl mt-44 " id="blue-fromDox-gsap"></div>
         </main>
     );
 };
