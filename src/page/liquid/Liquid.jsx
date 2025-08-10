4a25e77f04881ea099a332b6db53db79175bdcfb
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-08-10 15:58:57 +0600

    update the liquid with the background size

--- src/page/liquid/Liquid.jsx
+++ src/page/liquid/Liquid.jsx
@@ -2,8 +2,11 @@ import './liquid.css';
 
 const Liquid = () => {
     return (
-        <div>
-            <h2>Hello</h2>
+        <div className='main-body-liquid-bg'>
+            <div className='glass-container'>
+                <h2 className='font-bold text-2xl'>St. Basil's Cathedral</h2>
+                <p className='font-medium text-sm w-[30vw] '>St. Basil's Cathedral. It is located in Moscow, Russia, and is one of the country's most iconic landmarks. The cathedral is famous for its unique and vibrant onion domes and its distinctive architectural style.</p>
+            </div>
         </div>
     );
 };
