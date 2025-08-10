84444742e4938cadf5ed4e2669fdbcaf94d80bd9
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-08-10 16:20:46 +0600

    update the glass

--- src/page/liquid/Liquid.jsx
+++ src/page/liquid/Liquid.jsx
@@ -5,7 +5,7 @@ const Liquid = () => {
         <div className='main-body-liquid-bg'>
             <div className='glass-container'>
                 <h2 className='font-bold text-2xl'>St. Basil's Cathedral</h2>
-                <p className='font-medium text-sm w-[30vw] '>St. Basil's Cathedral. It is located in Moscow, Russia, and is one of the country's most iconic landmarks. The cathedral is famous for its unique and vibrant onion domes and its distinctive architectural style.</p>
+                <p className='font-medium text-sm w-[80%] '>St. Basil's Cathedral. It is located in Moscow, Russia, and is one of the country's most iconic landmarks. The cathedral is famous for its unique and vibrant onion domes and its distinctive architectural style.</p>
             </div>
         </div>
     );
